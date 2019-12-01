cdb7918ce155a9db106a5654810186dcbde5d5df
akurczak <akurczakgh@gmail.com>
2019-12-01 19:53:25 +0100

    Created Gradle project

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+group = "pl.kurczak.idea"
+version = "1.0-SNAPSHOT"
+
+plugins {
+    kotlin("jvm") version "1.3.61"
+    id("org.jetbrains.intellij").version("0.4.14")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
+
+intellij {
+    version = "IC-193.5233.102"
+    instrumentCode = false // See: https://github.com/JetBrains/gradle-intellij-plugin/issues/230
+//    setPlugins("intellij-dvcs")
+}
